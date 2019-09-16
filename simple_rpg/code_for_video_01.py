85ee14571e2c25d11b8c99782cb718e204f1030b
poly451 <49294069+poly451@users.noreply.github.com>
2019-09-16 09:44:20 -0700

    Update code_for_video_01.py

--- simple_rpg/code_for_video_01.py
+++ simple_rpg/code_for_video_01.py
@@ -1,1 +1,49 @@
+import pygame
+import sys
+
+TILESIZE = 64
+WIDTH = TILESIZE * 16
+HEIGHT = TILESIZE * 16
+RED = (255, 0, 0)
+
+# ----------------------------------------------------------------
+#                           class Game
+# ----------------------------------------------------------------
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption('My World')
+        self.display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.display_surface.fill(RED)
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.myquit()
+                return True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.myquit()
+                    return True
+
+    def myquit(self):
+        pygame.quit()
+        sys.exit()
+
+# ==================================================================
+# ==================================================================
+
+def setup():
+    mygame = Game()
+
+    while True:
+        mygame.handle_events()
+        # ----------------------------------
+        pygame.display.flip()
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    setup()
 
