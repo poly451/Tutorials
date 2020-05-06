5efaec1b1342a88242f295796ed73a42d0273568
poly451 <49294069+poly451@users.noreply.github.com>
2020-05-05 20:43:20 -0700

    Create part_01.py

---
+++ How to create a text adventure using Python/part_01.py
@@ -0,0 +1,11 @@
+"""
+What we want to accomplish:
+(a) Make sure everything works as it should.
+
+Yes! Let’s move on to the next step and implement the following functionality:
+"""
+def main_menu():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main_menu()
