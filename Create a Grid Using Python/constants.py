6c5076940f0a17aed8ff44d7f6c4f63d1e7c83b3
poly451 <49294069+poly451@users.noreply.github.com>
2020-07-30 00:26:11 -0700

    Create constants.py

---
+++ Create a Grid Using Python/constants.py
@@ -0,0 +1,49 @@
+# TITLE = "Checkers"
+# TILES_HORIZONTAL = 4
+# TILES_VERTICAL = 4
+# TILESIZE = 128
+# WINDOWN_WIDTH = TILESIZE * TILES_HORIZONTAL
+# WINDOW_HEIGHT = TILESIZE * TILES_VERTICAL
+# # ------------------------
+# WINDOW_OFFSET = 100
+# MAP_CHOICES = ["r_", "b_", "bo", "bx"]
+# ------------------------
+KIND_OF_TILES = ["yellow_square", "white_square", "red_checker"]
+KIND_OF_TILES += ["blue_checker", "blue_checker_drowned", "red_checker_crowned"]
+# ------------------------
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (200, 200, 200)
+DARK_GREEN = (  0, 153, 0)
+GREEN = (0, 255, 0)
+DARK_RED = (204, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 128, 0)
+YELLOW = (255, 255, 0)
+BROWN = (106, 55, 5)
+
+AQUA = (  0, 255, 255)
+BLUE = (  0,  0, 255)
+FUCHIA = (255,   0, 255)
+GRAY = (128, 128, 128)
+LIME = (  0, 255,   0)
+MAROON = (128,  0,   0)
+NAVY_BLUE = (  0,  0, 128)
+OLIVE = (128, 128,   0)
+PURPLE = (128,  0, 128)
+LIGHT_PURBLE = (255, 153, 255)
+SILVER = (192, 192, 192)
+TEAL = (  0, 128, 128)
+# ------------------------
+YELLOW_SQUARE = "yellow_square.png"
+WHITE_SQUARE = "white_square.png"
+RED_CHECKER = "checker_red_blank.png"
+BLUE_CHECKER = "checker_blue_blank.png"
+LIGHT_BLUE_CHECKER = "blue_checker_on_yellow.png"
+LIGHT_BLUE_CHECKER_CROWNED = "light_blue_crowned_checker.png"
+RED_CHECKER_CROWNED = "red_checker_crowned.png"
+BLUE_CHECKER_CROWNED = "blue_crowned_checker.png"
+GRASS = "grass02.gif"
+DIRT = "dirt_small_square.png"
+DOG = "dog02.png"
