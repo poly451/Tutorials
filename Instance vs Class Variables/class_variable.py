29106ca32af8feff977e22949d0a3707b295bd27
poly451 <49294069+poly451@users.noreply.github.com>
2020-08-03 11:21:00 -0700

    Create class_variable.py

---
+++ Instance vs Class Variables/class_variable.py
@@ -0,0 +1,24 @@
+class Car:
+    def __init__(self):
+        self.tires = 0
+        self.seats = 0
+        self.number_of_cars = 0
+
+    def add_car(self, tires, seats):
+        self.tires = tires
+        self.seats = seats
+
+    def print_car(self):
+        print("tires: {}, seats: {}".format(self.tires, self.seats))
+
+print("The first car: ")
+print("-" * 30)
+ford = Car()
+ford.add_car(tires=3, seats=4)
+ford.print_car()
+print("*" * 30)
+print("\nThe second car: ")
+print("-" * 30)
+porsche = Car()
+porsche.add_car(tires=4, seats=2)
+porsche.print_car()
