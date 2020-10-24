a8de223269f9b0226580ffcce26fb03504d1de87
poly451 <49294069+poly451@users.noreply.github.com>
2020-10-23 18:16:30 -0700

    Create constants.py

---
+++ Teleprompter/constants.py
@@ -0,0 +1,39 @@
+TITLE = "The World of Light and Shadow"
+ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+ALPHABET += ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+MISC = [",", "!", "#", "$", "%", "&", "*", "(", ")", "-", "_", "+", "=", "`", "~", ".", ";", ":", " ", "?", "@", "[", "]", "{", "}"]
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+CLEAN_CHARS = ALPHABET + MISC + NUMBERS
+ZONE_NAMES = ["harbor"]
+# ---------------------------------------------
+TILESIZE = 120
+TILES_WIDE = 4
+TILES_HIGH = 4
+WINDOW_WIDTH = TILESIZE * TILES_WIDE
+WINDOW_HEIGHT = TILESIZE * TILES_HIGH
+# ---------------------------------------------
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (200, 200, 200)
+DARK_GREEN = (  0, 153, 0)
+GREEN = (0, 255, 0)
+DARK_RED = (204, 0, 0)
+RED = (255, 0, 0)
+ORANGE = (255, 128, 0)
+YELLOW = (255, 255, 0)
+BROWN = (106, 55, 5)
+
+AQUA = (  0, 255, 255)
+BLUE = (  0,  0, 255)
+FUCHIA = (255,   0, 255)
+GRAY = (128, 128, 128)
+LIME = (  0, 255,   0)
+MAROON = (128,  0,   0)
+NAVY_BLUE = (  0,  0, 128)
+OLIVE = (128, 128,   0)
+PURPLE = (128,  0, 128)
+LIGHT_PURBLE = (255, 153, 255)
+SILVER = (192, 192, 192)
+TEAL = (  0, 128, 128)
+BACKGROUND_COLOR = WHITE
