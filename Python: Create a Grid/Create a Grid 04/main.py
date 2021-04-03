fcb8d563bcd6795780c790fbfe819a6cfbd942a5
poly451 <49294069+poly451@users.noreply.github.com>
2021-04-03 10:39:41 -0700

    Create main.py

---
+++ Python: Create a Grid/Create a Grid 04/main.py
@@ -0,0 +1,26 @@
+import pygame
+from myclasses import Game
+import constants
+import sys, os
+
+# ==================================================================
+# ==================================================================
+
+def setup():
+    mygame = Game()
+    # mygame.read_data()
+    # mygame.player.debug_print()
+    # mygame.monsters.debug_print()
+    # mygame.main()
+    # mygame.grasses.debug_print()
+    # mygame.walls.debug_print()
+
+
+    # while True:
+    #     mygame.handle_events()
+    #     mygame.draw_stuff()
+    #     pygame.display.update()
+
+
+if __name__ == "__main__":
+    setup()
